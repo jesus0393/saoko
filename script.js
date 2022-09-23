73822f79a3f0f2e18a9c27b0b81189a14aa43a97
Jesus Cordoba <jessusscj@gmail.com>
2022-09-23 14:31:23 -0500

    change: cambio de color mediante script

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ document.body.innerHTML= `<h1> hoy es dia de cholos y motomoames ${d}</h1>`;
 document.body.style.color = "red";*/
 
 console.log("hola")
+
+function cambiaColor(){ 
+    console.log("todo va bien");
+    document.getElementById("titulo").style.color = "red"
+}
