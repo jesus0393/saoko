339c78bcd46a54fa67263786ad93dd02c214a63a
Jesus Cordoba <jessusscj@gmail.com>
2022-09-26 11:00:37 -0500

    script dom

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ document.body.innerHTML= `<h1> hoy es dia de cholos y motomoames ${d}</h1>`;
 document.body.style.color = "red";*/
 
 console.log("hola")
-
+document.getElementById("titulo").style.color = "grey";
 function cambiaColor(){ 
     console.log("todo va bien");
-    document.getElementById("titulo").style.color = "red"
+    document.getElementById("titulo").style.color = "red";
 }
